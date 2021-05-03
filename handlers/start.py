1e660166be2b4b561ae186038603db3e97c3d785
Zaute Km <77600757+ZauteKm@users.noreply.github.com>
2021-05-04 00:34:29 +0530

    Update and rename private.py to start.py

---
+++ handlers/start.py
@@ -0,0 +1,115 @@
+# ZauteMusic (Telegram bot project )
+# Copyright (C) 2021  ZauteKm 
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+
+
+from pyrogram import Client, filters
+from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
+
+from config import BOT_NAME as bn
+
+
+
+
+@Client.on_message(filters.command("start") & filters.private & ~filters.channel)
+async def start(_, message: Message):
+    await message.reply_text(
+        f"""Hi there! I can play music in voice chats of Telegeam Groups. I have a lot of cool feature that will amaze you!\n\n♪ Do you want me to play music in your Telegram groups'voice chats? Please click the <b>\'Helpful Commands\'</b> button below to know how you can use me.\n\n♪ The Assistant must be in your group to play music in the voice chat of your group.\n\n♪ More info & commands mentioned in the [Helpful Commands.](https://telegra.ph/Group-Music-Bot-05-03)\n\nℹ️ Please Subscribe @ZauteKm For more Info.\n\n <b>Hit /help list of available Commands.</b>""",
+        reply_markup=InlineKeyboardMarkup(
+            [ 
+                [
+                    InlineKeyboardButton(
+                        "ℹ️ Helpful Commands ℹ️", url="https://telegra.ph/Group-Music-Bot-05-03")
+                  ],[
+                    InlineKeyboardButton(
+                        "🎙️ Group", url="https://t.me/joinchat/7gSUxv6vgQE3M2Fl"
+                    ),
+                    InlineKeyboardButton(
+                        "Channel 🔊", url="https://t.me/ZauteKm"
+                    )
+                ],[ 
+                    InlineKeyboardButton(
+                        "⚜️ YouTube Channel ⚜️", url="https://youtube.com/c/MizoHelpDesK"
+                    )]
+            ]
+        ),
+     disable_web_page_preview=True
+    )
+
+@Client.on_message(
+    filters.command("start")
+    & filters.group
+    & ~ filters.edited
+)
+async def start(client: Client, message: Message):
+    await message.reply_text(
+        "💁🏻‍♂️ Do you want to search for a YouTube video?",
+        reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton(
+                        "🔊 Channel", url="https://t.me/ZauteKm"
+                    )
+                ],    
+                [    
+                    InlineKeyboardButton(
+                        "✅ Yes", switch_inline_query_current_chat=""
+                    ),
+                    InlineKeyboardButton(
+                        "No ❌", callback_data="close"
+                    )
+                ]
+            ]
+        )
+    )
+
+@Client.on_message(
+    filters.command("help")
+    & filters.private
+    & ~ filters.edited
+)
+async def help(client: Client, message: Message):
+    await message.reply_text(
+        f"""<b><u>Helpful Commands!</u>
+\n/play <song name> - play song you requested
+/dplay <song name> - play song you requested via deezer
+/splay <song name> - play song you requested via jio saavn
+/playlist - Show now playing list
+/current - Show now playing
+/song <song name> - download songs you want quickly
+/search <query> - search videos on youtube with details
+/deezer <song name> - download songs you want quickly via deezer
+/saavn <song name> - download songs you want quickly via saavn
+/video <song name> - download videos you want quickly
+\n<u>Admins only</u>
+/player - open music player settings panel
+/pause - pause song play
+/resume - resume song play
+/skip - play next song
+/end - stop music play
+/userbotjoin - invite assistant to your chat
+/admincache - Refresh admin list
+ </b>""",
+        reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton(
+                        "🔊 Channel", url="https://t.me/ZauteKm"
+                    )
+                ]
+            ]
+        )
+    )    
