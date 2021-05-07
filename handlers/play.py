b9306d3a3ffc8057ea4fa6ca749fbb928fa7fe0a
Zaute Km <77600757+ZauteKm@users.noreply.github.com>
2021-05-07 09:20:12 +0530

    Update play.py

--- handlers/play.py
+++ handlers/play.py
@@ -776,7 +776,7 @@ async def jiosaavn(client: Client, message_: Message):
              [
                InlineKeyboardButton(
                    text="Join Updates Channel",
-                   url='https://t.me/daisyxupdates')
+                   url='https://t.me/ZauTeKm')
              ],
              [       
                InlineKeyboardButton(
