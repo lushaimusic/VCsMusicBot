11e7c5c86527e6cbd71594bffa2f1c30a268004e
Zaute Km <77600757+ZauteKm@users.noreply.github.com>
2021-05-14 21:56:49 +0530

    Update chat_member_updated.py

--- handlers/chat_member_updated.py
+++ handlers/chat_member_updated.py
@@ -22,23 +22,23 @@ from cache.admins import admins
 from pyrogram import Client
 
 @Client.on_chat_member_updated()
-async def auto_admin_updater(_, chat_member_updated: ChatMemberUpdated):
-    (
-        admins[chat_member_updated.chat.id].append(
-            chat_member_updated.new_chat_member.user.id
-        )
-    ) if (
+async def chat_member_updated(_, chat_member_updated: ChatMemberUpdated):
+    if chat_member_updated.new_chat_member and chat_member_updated.old_chat_member:
         (
-            (
-                chat_member_updated.new_chat_member.user.id
-            ) not in admins[chat_member_updated.chat.id]
-        )
-    ) else (
-        admins[chat_member_updated.chat.id].remove(
-            chat_member_updated.new_chat_member.user.id
-        )
-    ) if (
-        (
-            chat_member_updated.new_chat_member.user.id
-        ) in admins[chat_member_updated.chat.id]
-    ) else None
+            admins.admins[chat_member_updated.chat.id].append(
+                chat_member_updated.new_chat_member.user.id,
+            )
+        ) if (
+            (chat_member_updated.new_chat_member.can_manage_voice_chats)
+            and (
+                (chat_member_updated.new_chat_member.user.id)
+                not in admins.admins[chat_member_updated.chat.id]
+            )
+        ) else (
+            admins.admins[chat_member_updated.chat.id].remove(
+                chat_member_updated.new_chat_member.user.id,
+            )
+        ) if (
+            (chat_member_updated.new_chat_member.user.id)
+            in admins.admins[chat_member_updated.chat.id]
+        ) else None
