b3cdde22ce667e9995e2807a02c8d9fd37251b33
Zaute Km <77600757+ZauteKm@users.noreply.github.com>
2021-05-24 21:40:28 +0530

    Create config.py

---
+++ MusicBot/config.py
@@ -0,0 +1,44 @@
+# MUSICBOT- Telegram bot project
+# Copyright (C) 2021  Roj Serbest
+# Copyright (C) 2021  Zaute Km
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+# Modified by Inukaasith
+
+import os
+from os import getenv
+
+from dotenv import load_dotenv
+
+if os.path.exists("local.env"):
+    load_dotenv("local.env")
+
+que = {}
+SESSION_NAME = getenv("SESSION_NAME", "session")
+BOT_TOKEN = getenv("BOT_TOKEN")
+BOT_NAME = getenv("BOT_NAME")
+UPDATES_CHANNEL = getenv("UPDATES_CHANNEL", "ZauteKm
+BG_IMAGE = getenv("BG_IMAGE", "https://telegra.ph/file/0c26d3784bbcec0b3cf39.png")
+admins = {}
+API_ID = int(getenv("API_ID"))
+API_HASH = getenv("API_HASH")
+BOT_USERNAME = getenv("BOT_USERNAME")
+ASSISTANT_NAME = getenv("ASSISTANT_NAME", "ZauteMusicPlayer")
+SUPPORT_GROUP = getenv("SUPPORT_GROUP", "tgVCSets
+PROJECT_NAME = getenv("PROJECT_NAME", "Group Music Bot v4")
+SOURCE_CODE = getenv("SOURCE_CODE", "github.com/zautekm/GroupMusicBot")
+DURATION_LIMIT = int(getenv("DURATION_LIMIT", "7"))
+ARQ_API_KEY = getenv("ARQ_API_KEY", "GUGFNB-XPUZDX-XGCKTJ-UZXHBZ-ARQ")
+COMMAND_PREFIXES = list(getenv("COMMAND_PREFIXES", "/ !").split())
+
+SUDO_USERS = list(map(int, getenv("SUDO_USERS").split()))
