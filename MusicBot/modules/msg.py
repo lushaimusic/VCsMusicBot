1500e57d7280c731712eb506361266463c29e8ef
Zaute Km <77600757+ZauteKm@users.noreply.github.com>
2021-05-24 22:20:03 +0530

    Update msg.py

--- MusicBot/modules/msg.py
+++ MusicBot/modules/msg.py
@@ -1,5 +1,5 @@
 import os
-from DaisyXMusic.config import SOURCE_CODE,ASSISTANT_NAME,PROJECT_NAME,SUPPORT_GROUP,UPDATES_CHANNEL
+from MusicBot.config import SOURCE_CODE,ASSISTANT_NAME,PROJECT_NAME,SUPPORT_GROUP,UPDATES_CHANNEL
 class Messages():
       START_MSG = "**Hello 👋 [{}](tg://user?id={})!**\n\n🤖 I am an advanced bot created for playing music in the voice chats of Telegram Groups & Channels.\n\n✅ Send me /help for more info."
       HELP_MSG = [
