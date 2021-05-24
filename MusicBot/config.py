5b30e2070addbb371159410af01cc64396ceae59
Zaute Km <77600757+ZauteKm@users.noreply.github.com>
2021-05-24 23:05:39 +0530

    Update config.py

--- MusicBot/config.py
+++ MusicBot/config.py
@@ -1,20 +1,3 @@
-# MUSICBOT- Telegram bot project
-# Copyright (C) 2021  Roj Serbest
-# Copyright (C) 2021  Zaute Km
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU Affero General Public License as
-# published by the Free Software Foundation, either version 3 of the
-# License, or (at your option) any later version.
-
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU Affero General Public License for more details.
-#
-# You should have received a copy of the GNU Affero General Public License
-# along with this program.  If not, see <https://www.gnu.org/licenses/>.
-# Modified by Inukaasith
-
 import os
 from os import getenv
 
@@ -27,7 +10,7 @@ que = {}
 SESSION_NAME = getenv("SESSION_NAME", "session")
 BOT_TOKEN = getenv("BOT_TOKEN")
 BOT_NAME = getenv("BOT_NAME")
-UPDATES_CHANNEL = getenv("UPDATES_CHANNEL", "ZauteKm
+UPDATES_CHANNEL = getenv("UPDATES_CHANNEL", "ZauteKm")
 BG_IMAGE = getenv("BG_IMAGE", "https://telegra.ph/file/0c26d3784bbcec0b3cf39.png")
 admins = {}
 API_ID = int(getenv("API_ID"))
