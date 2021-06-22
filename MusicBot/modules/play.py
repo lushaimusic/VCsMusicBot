7609567c9bf553f4e2ac3ef7e35dc72de9aa8f94
Zaute <77600757+Zaute-Km@users.noreply.github.com>
2021-06-22 16:16:50 +0530

    Update play.py

--- MusicBot/modules/play.py
+++ MusicBot/modules/play.py
@@ -34,42 +34,6 @@ from MusicBot.services.callsmusic.callsmusic import client as USER
 from MusicBot.services.converter.converter import convert
 from MusicBot.services.downloaders import youtube
 
-import json
-import os
-from os import path
-from typing import Callable
-
-import aiofiles
-import aiohttp
-import ffmpeg
-import requests
-import wget
-from PIL import Image, ImageDraw, ImageFont
-from pyrogram import Client, filters
-from pyrogram.types import Voice
-from pyrogram.errors import UserAlreadyParticipant
-from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message
-from Python_ARQ import ARQ
-from youtube_search import YoutubeSearch
-
-from DaisyXMusic.config import ARQ_API_KEY
-from DaisyXMusic.config import BOT_NAME as bn
-from DaisyXMusic.config import DURATION_LIMIT
-from DaisyXMusic.config import UPDATES_CHANNEL as updateschannel
-from DaisyXMusic.config import que
-from DaisyXMusic.function.admins import admins as a
-from DaisyXMusic.helpers.admins import get_administrators
-from DaisyXMusic.helpers.channelmusic import get_chat_id
-from DaisyXMusic.helpers.errors import DurationLimitError
-from DaisyXMusic.helpers.decorators import errors
-from DaisyXMusic.helpers.decorators import authorized_users_only
-from DaisyXMusic.helpers.filters import command, other_filters
-from DaisyXMusic.helpers.gets import get_file_name
-from DaisyXMusic.services.callsmusic import callsmusic, queues
-from DaisyXMusic.services.callsmusic.callsmusic import client as USER
-from DaisyXMusic.services.converter.converter import convert
-from DaisyXMusic.services.downloaders import youtube
-
 aiohttpsession = aiohttp.ClientSession()
 chat_id = None
 arq = ARQ("https://thearq.tech", ARQ_API_KEY, aiohttpsession)
