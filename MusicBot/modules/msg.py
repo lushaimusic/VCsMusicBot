b01817f92c7c4968f4fd3f7a477d7d8158f00ee3
Zaute <77600757+Zaute-Km@users.noreply.github.com>
2021-06-22 18:16:04 +0530

    Update msg.py

--- MusicBot/modules/msg.py
+++ MusicBot/modules/msg.py
@@ -5,11 +5,11 @@ class Messages():
       HELP_MSG = [
         ".",
 f"""
-**Hey 👋 Welcome back to {PROJECT_NAME}
+**Hello, Welcome to {PROJECT_NAME}
 
-⭕ {PROJECT_NAME} can play music in your group's voice chat as well as channel voice chats.
+⭕ I can play music in your group's voice chat as well as channel voice chats.
 
-⭕ Assistant name >> @{ASSISTANT_NAME}\n\nClick next for instructions.**
+⭕ Assistant name: @{ASSISTANT_NAME}\n\nClick next for instructions.**
 """,
 
 f"""
@@ -17,34 +17,33 @@ f"""
 
 1) Make bot admin (Group and in channel if use cplay)
 2) Start a voice chat
-3) Try /play [song name] for the first time by an admin
- If userbot joined enjoy music, If not add @{ASSISTANT_NAME} to your group and retry
+3) Try /play `[song name]` for the first time by an admin
+ If userbot joined enjoy music, If not add @{ASSISTANT_NAME} to your group and retry.
 
 **For Channel Music Play**
-1) Make me admin of your channel 
-2) Send /userbotjoinchannel in linked group
-3) Now send commands in linked group
+1) Make me admin of your channel.
+2) Send /userbotjoinchannel in linked group.
+3) Now send commands in linked group.
 
 **Commands**
 
 **=>> Song Playing 🎧**
 
-- /play: Play song using youtube music
-- /play [yt url] : Play the given yt url
-- /play [reply audio]: Play replied audio
-- /dplay: Play song via deezer
-- /splay: Play song via jio saavn
-- /ytplay: Directly play song via YouTube Music
+- /play `[song name]`: Select the Given Below Keyboard.
+- /play `[yt url]` : Play the given YouTube URL.
+- /ytplay: Directly play song via YouTube Music.
+- /dplay: Play song via deezer.
+- /splay: Play song via jio saavn.
 
 **=>> Playback ⏯**
 
-- /player: Open Settings menu of player
-- /skip: Skips the current track
-- /pause: Pause track
-- /resume: Resumes the paused track
-- /end: Stops media playback
-- /current: Shows the current Playing track
-- /playlist: Shows playlist
+- /player: Open Settings menu of player.
+- /skip: Skips the current track.
+- /pause: Pause track.
+- /resume: Resumes the paused track.
+- /end: Stops media playback.
+- /current: Shows the current Playing track.
+- /playlist: Shows playlist.
 
 **Player cmd and all other cmds except /play, /current  and /playlist  are only for admins of the group.**
 """,
@@ -54,17 +53,17 @@ f"""
 
 **⭕ For linked group admins only:**
 
-- /cplay [song name] - play song you requested
-- /cdplay [song name] - play song you requested via deezer
-- /csplay [song name] - play song you requested via jio saavn
-- /cplaylist - Show now playing list
-- /cccurrent - Show now playing
-- /cplayer - open music player settings panel
-- /cpause - pause song play
-- /cresume - resume song play
-- /cskip - play next song
-- /cend - stop music play
-- /userbotjoinchannel - invite assistant to your chat
+- /cplay `[song name]`: Play song you requested.
+- /cdplay `[song name]`: Play song you requested via deezer.
+- /csplay `[song name]`: Play song you requested via jio saavn.
+- /cplaylist: Show now playing list.
+- /cccurrent: Show now playing.
+- /cplayer: Open music player settings panel.
+- /cpause: Pause song play.
+- /cresume: Resume song play.
+- /cskip: Play next song.
+- /cend: Stop music play.
+- /userbotjoinchannel: Invite assistant to your chat.
 
 **Channel is also can be used instead of c** ( /cplay = /channelplay )
 
@@ -80,16 +79,16 @@ f"""
 f"""
 **=>> More tools 😬**
 
-- /musicplayer [on/off]: Enable/Disable Music player
+- /musicplayer `<on/off>`: Enable/Disable Music player
 - /reload: Updates admin info of your group. Try if bot isn't recognize admin
 - /userbotjoin: Invite @{ASSISTANT_NAME} Userbot to your chat
 
 **=>> Commands for Sudo Users 👷**
 
- - /userbotleaveall - remove assistant from all chats
- - /gcast <reply to message> - globally brodcast replied message to all chats
- - /pmpermit [on/off] - enable/disable pmpermit message
-*Sudo Users can execute any command in any groups
+ - /userbotleaveall - Remove assistant from all chats.
+ - /gcast `<reply to message>` - Globally brodcast replied message to all chats.
+ - /pmpermit `<on/off>` - Enable/Disable PM Permit message.
+**Sudo Users can execute any command in any groups.**
 
 """
       ]
