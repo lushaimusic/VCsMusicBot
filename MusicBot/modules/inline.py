70636e50366bd5194d37bce3d2633e0608bd3f90
Zaute <77600757+Zaute-Km@users.noreply.github.com>
2021-06-22 20:42:26 +0530

    Update inline.py

--- MusicBot/modules/inline.py
+++ MusicBot/modules/inline.py
@@ -1,48 +1,59 @@
-from pyrogram import Client, errors
-from pyrogram.types import (
-    InlineQuery,
-    InlineQueryResultArticle,
-    InputTextMessageContent,
-)
+from pyrogram.handlers import InlineQueryHandler
 from youtubesearchpython import VideosSearch
+from pyrogram.types import InlineQueryResultArticle, InputTextMessageContent
+from pyrogram import Client, errors
 
 
 @Client.on_inline_query()
-async def inline(client: Client, query: InlineQuery):
+async def search(client, query):
     answers = []
-    search_query = query.query.lower().strip().rstrip()
+    string = query.query.lower().strip().rstrip()
 
-    if search_query == "":
+    if string == "":
         await client.answer_inline_query(
             query.id,
             results=answers,
-            switch_pm_text="Type a YouTube video name...",
+            switch_pm_text=("Search a youtube video"),
             switch_pm_parameter="help",
-            cache_time=0,
+            cache_time=0
         )
+        return
     else:
-        search = VideosSearch(search_query, limit=50)
-
-        for result in search.result()["result"]:
+        videosSearch = VideosSearch(string.lower(), limit=50)
+        for v in videosSearch.result()["result"]:
             answers.append(
                 InlineQueryResultArticle(
-                    title=result["title"],
-                    description="{}, {} views.".format(
-                        result["duration"], result["viewCount"]["short"]
+                    title=v["title"],
+                    description=("Duration: {} Views: {}").format(
+                        v["duration"],
+                        v["viewCount"]["short"]
                     ),
                     input_message_content=InputTextMessageContent(
-                        "/play https://www.youtube.com/watch?v={}".format(result["id"])
+                        "/play https://www.youtube.com/watch?v={}".format(
+                            v["id"]
+                        )
                     ),
-                    thumb_url=result["thumbnails"][0]["url"],
+                    thumb_url=v["thumbnails"][0]["url"]
                 )
             )
-
         try:
-            await query.answer(results=answers, cache_time=0)
+            await query.answer(
+                results=answers,
+                cache_time=0
+            )
         except errors.QueryIdInvalid:
             await query.answer(
                 results=answers,
                 cache_time=0,
-                switch_pm_text="Error: Search timed out",
+                switch_pm_text=("Nothing found"),
                 switch_pm_parameter="",
             )
+
+
+__handlers__ = [
+    [
+        InlineQueryHandler(
+            search
+        )
+    ]
+]
