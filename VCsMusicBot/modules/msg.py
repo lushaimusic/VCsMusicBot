41608a483979a155fcda41018f7dcc356fd6fc66
Zaute <77600757+Zaute-Km@users.noreply.github.com>
2021-06-23 16:34:43 +0530

    Update and rename MusicBot/modules/msg.py to VCsMusicBot/modules/msg.py

--- VCsMusicBot/modules/msg.py
+++ VCsMusicBot/modules/msg.py
@@ -1,5 +1,5 @@
 import os
-from MusicBot.config import SOURCE_CODE,ASSISTANT_NAME,PROJECT_NAME,SUPPORT_GROUP,UPDATES_CHANNEL
+from VCsMusicBot.config import SOURCE_CODE,ASSISTANT_NAME,PROJECT_NAME,SUPPORT_GROUP,UPDATES_CHANNEL
 class Messages():
       START_MSG = "**Hello 👋 [{}](tg://user?id={})!**\n\n🤖 I am an advanced bot created for playing music in the voice chats of Telegram Groups & Channels.\n\n✅ Hits /help for more info."
       HELP_MSG = [
