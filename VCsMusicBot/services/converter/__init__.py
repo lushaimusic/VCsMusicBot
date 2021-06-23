51ede84051f925b74266387df699548cf08240e0
Zaute <77600757+Zaute-Km@users.noreply.github.com>
2021-06-23 16:15:37 +0530

    Update and rename MusicBot/services/converter/__init__.py to VCsMusicBot/services/converter/__init__.py

--- VCsMusicBot/services/converter/__init__.py
+++ VCsMusicBot/services/converter/__init__.py
@@ -3,4 +3,4 @@ from os import listdir, mkdir
 if "raw_files" not in listdir():
     mkdir("raw_files")
 
-from MusicBot.services.converter.converter import convert
+from VCsMusicBot.services.converter.converter import convert
