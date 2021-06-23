ef348054e81e60678be611c4721c43a181308d4f
Zaute <77600757+Zaute-Km@users.noreply.github.com>
2021-06-23 16:44:27 +0530

    Update and rename MusicBot/modules/private.py to VCsMusicBot/modules/private.py

--- VCsMusicBot/modules/private.py
+++ VCsMusicBot/modules/private.py
@@ -1,8 +1,8 @@
 import logging
-from MusicBot.modules.msg import Messages as tr
+from VCsMusicBot.modules.msg import Messages as tr
 from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, Message
-from MusicBot.config import SOURCE_CODE,ASSISTANT_NAME,PROJECT_NAME,SUPPORT_GROUP,UPDATES_CHANNEL,BOT_USERNAME
+from VCsMusicBot.config import SOURCE_CODE,ASSISTANT_NAME,PROJECT_NAME,SUPPORT_GROUP,UPDATES_CHANNEL,BOT_USERNAME
 logging.basicConfig(level=logging.INFO)
 
 @Client.on_message(filters.private & filters.incoming & filters.command(['start']))
