8866b85d342ee0a8f43f4df258bc637e60c83ee4
Zaute <77600757+Zaute-Km@users.noreply.github.com>
2021-06-23 16:31:43 +0530

    Update and rename MusicBot/modules/chat_member_updated.py to VCsMusicBot/modules/chat_member_updated.py

--- VCsMusicBot/modules/chat_member_updated.py
+++ VCsMusicBot/modules/chat_member_updated.py
@@ -1,7 +1,7 @@
 from pyrogram import Client
 from pyrogram.types import ChatMemberUpdated
 
-from MusicBot.function import *
+from VCsMusicBot.function import *
 
 
 @Client.on_chat_member_updated()
