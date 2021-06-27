62afcfc7793840e4f62295efd20a8bc889b2a950
Zaute <77600757+Zaute-Km@users.noreply.github.com>
2021-06-28 00:16:59 +0530

    Update __init__.py

--- VCsMusicBot/function/__init__.py
+++ VCsMusicBot/function/__init__.py
@@ -1,3 +1,5 @@
-from VCsMusicBot.function.admins import admins, get, set
+from VCsMusicBot.function.admins import admins
+from VCsMusicBot.function.admins import get
+from VCsMusicBot.function.admins import set
 
 __all__ = ["set", "get", "admins"]
