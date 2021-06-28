9a079b1939333ce683776deb7e737eb5d61df282
TEX-X <83172693+sahil757755@users.noreply.github.com>
2021-06-28 22:46:24 +0530

    Update config.py

--- VCsMusicBot/config.py
+++ VCsMusicBot/config.py
@@ -22,7 +22,7 @@ SOURCE_CODE = getenv("SOURCE_CODE", "github.com/LushaiMusic/VCsMusicBot")
 DURATION_LIMIT = int(getenv("DURATION_LIMIT", "7"))
 ARQ_API_KEY = getenv("ARQ_API_KEY", None)
 PMPERMIT = getenv("PMPERMIT", None)
-LOG_GRP = getenv("LOG_GRP", None)
+
 COMMAND_PREFIXES = list(getenv("COMMAND_PREFIXES", "/ !").split())
 
 SUDO_USERS = list(map(int, getenv("SUDO_USERS").split()))
