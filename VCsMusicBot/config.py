197a507d1cb5ac8365baa0ce2d0c5bb53a84d6c9
Zaute <77600757+ZauteKm@users.noreply.github.com>
2021-07-07 21:29:09 +0530

    Update config.py

--- VCsMusicBot/config.py
+++ VCsMusicBot/config.py
@@ -1,5 +1,6 @@
-from os import getenv
 import os
+from os import path
+from os import getenv
 from dotenv import load_dotenv
 
 if os.path.exists("local.env"):
@@ -9,20 +10,19 @@ que = {}
 SESSION_NAME = getenv("SESSION_NAME", "session")
 BOT_TOKEN = getenv("BOT_TOKEN")
 BOT_NAME = getenv("BOT_NAME")
-UPDATES_CHANNEL = getenv("UPDATES_CHANNEL", "ZauteKm")
+UPDATES_CHANNEL = getenv("UPDATES_CHANNEL", "tgbotproject")
 BG_IMAGE = getenv("BG_IMAGE", "https://telegra.ph/file/cf19dda907391656338eb.png")
 admins = {}
-API_ID = int(getenv("API_ID"))
+API_ID = int(getenv("API_ID", ""))
 API_HASH = getenv("API_HASH")
 BOT_USERNAME = getenv("BOT_USERNAME")
 ASSISTANT_NAME = getenv("ASSISTANT_NAME", "VCsMusicPlayer")
-SUPPORT_GROUP = getenv("SUPPORT_GROUP", "tgVCSets")
+SUPPORT_GROUP = getenv("SUPPORT_GROUP", "zauteschat")
 PROJECT_NAME = getenv("PROJECT_NAME", "VCsMusicBot")
-SOURCE_CODE = getenv("SOURCE_CODE", "github.com/LushaiMusic/VCsMusicBot")
-DURATION_LIMIT = int(getenv("DURATION_LIMIT", "7"))
+SOURCE_CODE = getenv("SOURCE_CODE", "github.com/ZauteKm/VCsMusicBot")
+DURATION_LIMIT = int(getenv("DURATION_LIMIT", "15"))
 ARQ_API_KEY = getenv("ARQ_API_KEY", None)
 PMPERMIT = getenv("PMPERMIT", None)
 LOG_GRP = getenv("LOG_GRP", None)
 COMMAND_PREFIXES = list(getenv("COMMAND_PREFIXES", "/ !").split())
-
 SUDO_USERS = list(map(int, getenv("SUDO_USERS").split()))
