5f69cf76b214de08a42940a6fa41e1c50b962781
Zaute <77600757+ZauteKm@users.noreply.github.com>
2021-07-28 06:18:52 +0530

    Update __init__.py

--- VCsMusicBot/services/callsmusic/__init__.py
+++ VCsMusicBot/services/callsmusic/__init__.py
@@ -1,6 +1,4 @@
-from pyrogram import Client
+from VCsMusicBot.services.queues import queues
+from VCsMusicBot.services.callsmusic.callsmusic import pytgcalls, run
 
-from VCsMusicBot import config
-
-client = Client(config.SESSION_NAME, config.API_ID, config.API_HASH)
-run = client.run
+__all__ = ["queues", "pytgcalls", "run"]
